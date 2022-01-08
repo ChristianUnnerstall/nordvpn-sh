e0b04c2fad593364176f896be94507e995be061c
Christian Unnerstall <76201962+ChristianUnnerstall@users.noreply.github.com>
2022-01-08 11:52:33 +0100

    Update on available options
    Update on available options

--- nordvpn.sh
+++ nordvpn.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
 
-options=("nordvpn connect" "nordvpn disconnect" "nordvpn settings" "nordvpn status")
+# https://support.nordvpn.com/de/Konnektivitaet/Linux/1569819582/NordVPN-unter-Debian-Ubuntu-Raspberry-Pi-Elementary-OS-und-Linux-Mint-installieren-und-verwenden.htm
+
+options=("nordvpn login" "nordvpn logout" "nordvpn account" "nordvpn connect" "nordvpn disconnect" "nordvpn countries" "nordvpn settings" "nordvpn status")
 
 select opt in "${options[@]}" "Quit"
 do
 	clear
 	case "$REPLY" in
 		1)
+			echo "nordvpn login"
+			nordvpn login
+			;;
+		2)
+			echo "nordvpn logout"
+			nordvpn logout
+			;;
+		3)
+			echo "nordvpn account"
+			nordvpn account
+			;;
+		4)
 			echo "nordvpn connect"
 			nordvpn connect
 			;;
-		2)
+		5)
 			echo "nordvpn disconnect"
 			nordvpn disconnect
 			;;
-        3)
+		6)
+			echo "nordvpn countries"
+			nordvpn countries
+			;;
+		7)
 			echo "nordvpn settings"
 			nordvpn settings
 			;;
-        4)
+        8)
 			echo "nordvpn status"
 			nordvpn status
 			;;
-		5)
+		9)
 			break
 			;;
 		*) echo "invalid option $REPLY";continue;;
