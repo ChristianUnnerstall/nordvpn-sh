a585561701ae8a1d6ec102e4b4fb41c6dbdbb2eb
Christian Unnerstall <76201962+ChristianUnnerstall@users.noreply.github.com>
2022-01-10 19:38:08 +0100

    Add disable/enable all settings

--- nordvpn.sh
+++ nordvpn.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# https://support.nordvpn.com/de/Konnektivitaet/Linux/1569819582/NordVPN-unter-Debian-Ubuntu-Raspberry-Pi-Elementary-OS-und-Linux-Mint-installieren-und-verwenden.htm
-
-options=("nordvpn login" "nordvpn logout" "nordvpn account" "nordvpn connect" "nordvpn disconnect" "nordvpn countries" "nordvpn settings" "nordvpn status")
+options=("nordvpn login" "nordvpn logout" "nordvpn account" "nordvpn connect" "nordvpn disconnect" "nordvpn Disable All" "nordvpn Enable All" "nordvpn countries" "nordvpn settings" "nordvpn status")
 
 select opt in "${options[@]}" "Quit"
 do
@@ -29,18 +27,42 @@ do
 			nordvpn disconnect
 			;;
 		6)
+			echo "nordvpn disable everything"
+			nordvpn disconnect
+			nordvpn set killswitch false
+			nordvpn set firewall false
+			nordvpn set obfuscate false
+			nordvpn set cybersec false
+			nordvpn set notify false
+			nordvpn set autoconnect false
+			nordvpn set ipv6 false
+			nordvpn connect
+			;;
+		7)
+			echo "nordvpn enable everything"
+			nordvpn disconnect
+			nordvpn set firewall true
+			nordvpn set killswitch true
+			nordvpn set obfuscate true
+			nordvpn set cybersec true
+			nordvpn set notify true
+			nordvpn set autoconnect true
+			nordvpn set ipv6 true
+			nordvpn connect
+			;;
+		8)
 			echo "nordvpn countries"
 			nordvpn countries
 			;;
-		7)
+		9)
 			echo "nordvpn settings"
 			nordvpn settings
 			;;
-        8)
+        10)
 			echo "nordvpn status"
 			nordvpn status
 			;;
-		9)
+		11)
 			break
 			;;
 		*) echo "invalid option $REPLY";continue;;
